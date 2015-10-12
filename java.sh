9a3dc7ddde5a318167b80bfd082fdde5a1a03818
zuchunlei <zuchunlei@outlook.com>
2015-10-12 10:51:51 +0800

    * 添加jdk配置的相关注释信息。

--- java.sh
+++ java.sh
@@ -1,3 +1,6 @@
+############################################
+####### Set JAVA Environment Setting #######
+############################################
 #!/bin/bash 
 export JAVA_HOME=/usr/java/jdk1.6.0_45
 export CLASS_PATH=.:$JAVA_HOME/lib/tools.jar
